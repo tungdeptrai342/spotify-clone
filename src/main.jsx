9edd14f185d7a4822c9b934cfbaa911d1930da09
tungnb123 <tungnb0402@gmail.com>
2024-08-12 21:58:51 +0700

    add search

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import "./style.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ApiProvider } from "./Contex/index.tsx";
 import { SearchProvider } from "./Contex/searchContext.tsx";
-import SearchPage from "./Pages/SearchPage/index.tsx";
-import SearchInput from "./components/SearchComponent/index.tsx";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -15,8 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <SearchProvider>
       <QueryClientProvider client={queryClient}>
         <App />
-        <SearchInput />
-        <SearchPage />
       </QueryClientProvider>
       </SearchProvider>
     </ApiProvider>
